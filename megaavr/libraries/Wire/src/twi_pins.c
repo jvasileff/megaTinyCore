0f5a5ead0c9eadb91db8e0a55bba3585b0f2ba81
John Vasileff <john@vasileff.com>
2024-11-05 19:22:55 -0500

    Fix i2c pullups on 2-series per bug 1158

--- megaavr/libraries/Wire/src/twi_pins.c
+++ megaavr/libraries/Wire/src/twi_pins.c
@@ -438,7 +438,7 @@ void TWI0_usePullups() {
         port->PIN3CTRL |= PORT_PULLUPEN_bm;
       }
     #endif
-  #elif defined(MEGATINYCORE) && MEGATINYCORE_SERIES != 2  /* tinyAVR 0/1-series */
+  #elif defined(MEGATINYCORE)
     #if defined(PORTMUX_TWI0_bm) // 1-series with remappable TWI
       if (true == (PORTMUX.CTRLB & PORTMUX_TWI0_bm)) {
         PORTA.OUTCLR    = 0x06;
